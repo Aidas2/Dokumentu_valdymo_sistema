4da7e0e76f90bd79ad1f3d6547bdf0cc746504b3
Vilius Zalys <you@example.com>
2019-01-31 22:51:28 +0200

    added some Group services

--- dvs-spring/src/main/java/lt/akademijait/bronza/controllers/UserController.java
+++ dvs-spring/src/main/java/lt/akademijait/bronza/controllers/UserController.java
@@ -2,14 +2,16 @@ package lt.akademijait.bronza.controllers;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lt.akademijait.bronza.dto.user.UserCreateCommand;
 import lt.akademijait.bronza.dto.user.UserGetCommand;
 import lt.akademijait.bronza.services.DocumentService;
 import lt.akademijait.bronza.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -28,8 +30,12 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-
-
+//    @RequestMapping(method = RequestMethod.POST)
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @ApiOperation(value = "Create new user", notes = "Creates new user")
+//    public void createUser (@ApiParam(value = "User data", required = true) @Valid @RequestBody final UserCreateCommand ucc){
+//        userService.createNewUser(ucc.getFirstName(), ucc.getLastName(), ucc.getUsername(), ucc.getPassword(), ucc.getEmailAddress());
+//    }
 
 
 
--- dvs-spring/src/main/java/lt/akademijait/bronza/services/UserGroupService.java
+++ dvs-spring/src/main/java/lt/akademijait/bronza/services/UserGroupService.java
@@ -1,12 +1,35 @@
 package lt.akademijait.bronza.services;
 
+import lt.akademijait.bronza.dto.usergroup.UserGroupGetCommand;
+import lt.akademijait.bronza.entities.DocType;
+import lt.akademijait.bronza.entities.UserGroup;
 import lt.akademijait.bronza.repositories.UserGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserGroupService {
 
     @Autowired
     private UserGroupRepository userGroupRepository;
+
+    @Transactional
+    public List<UserGroupGetCommand> getAllGroups(){
+        return userGroupRepository.findAll().stream().map((userGroup) ->
+                new UserGroupGetCommand(userGroup.getTitle())).collect(Collectors.toList());
+    }
+
+
+    @Transactional
+    public void createNewGroup (String title, List<DocType> submissionDocType, List<DocType> reviewDocType){
+        UserGroup newGroup = new UserGroup(title, submissionDocType, reviewDocType);
+        userGroupRepository.save(newGroup);
+
+    }
+
+
 }
