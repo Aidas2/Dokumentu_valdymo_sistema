3b87a75db542093afd03a8e95247f6cda591f52f
Paulius <paulius.ccs@gmail.com>
2019-02-06 16:57:26 +0200

    FileUpload was modified to work with windows and Linux on any computer

--- dvs-react/src/components/FileUpload/FileUploadComponent.js
+++ dvs-react/src/components/FileUpload/FileUploadComponent.js
@@ -3,15 +3,18 @@ import PropTypes from "prop-types";
 
 const FileUploadComponent = props => {
   return (
-    <div className="App">
+    <div className="container-fluid">
       <h3>Dokumento įkėlimas</h3>
       <form>
         <div className="">
-          <label className="m-2">Documento pavadinimas</label>
+          <label className="m-2 ">Dokumento pavadinimas</label>
           <br />
-          <input className="m-2" type="text" onChange={props.onDocumentTitle} />
-          <br />
-          <label>Dokumento tipas</label>
+          <input
+            className="m-2 form-control col-2"
+            type="text"
+            onChange={props.onDocumentTitle}
+          />
+          <label className="m-2">Dokumento tipas</label>
           <select
             //   onLoad={props.onInitialDocumentType}
             onChange={props.onDocumentType}
@@ -24,18 +27,51 @@ const FileUploadComponent = props => {
             <option>Type4</option>
             <option>Type5</option>
           </select>
-          <label>Pasirinkite dokumentą</label>&nbsp;
-          <br />
-          <input type="file" name="file" onChange={props.onFile} />
         </div>
-        <div className="">
+
+        <div className="input-group mb-3 mt-3 col-3  pl-2">
+          <div className="input-group-prepend" />
+          <div className="custom-file">
+            <input
+              type="file"
+              onChange={props.onFile}
+              className="custom-file-input "
+              id="inputGroupFile01"
+            />
+            <label className="custom-file-label" form="inputGroupFile01">
+              Pasirinkite pagrindinį dokumentą
+            </label>
+          </div>
+        </div>
+
+        <div className="input-group mb-3 mt-3 col-3  pl-2">
+          <div className="input-group-prepend" />
+          <div className="custom-file">
+            <input
+              type="file"
+              multiple
+              onChange={props.onFile}
+              className="custom-file-input "
+              id="inputGroupFile01"
+            />
+            <label className="custom-file-label" form="inputGroupFile01">
+              Pasirinkite paildomus failus{" "}
+            </label>
+          </div>
+        </div>
+
+        {/* <div className="m-2">
           <label>Pasirinkite paildomus failus</label>&nbsp;
           <br />
           <input type="file" multiple name="file" onChange={props.onFile} />
         </div>
-        <br />
+        <br /> */}
 
-        <button type="button" className="btn btn-info" onClick={props.onUpload}>
+        <button
+          type="button"
+          className="btn btn-info m-2"
+          onClick={props.onUpload}
+        >
           Įkelti
         </button>
       </form>
