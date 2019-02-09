e66914ca7aed91151e1cc8403150c8d7e6283306
Paulius <paulius.ccs@gmail.com>
2019-02-09 13:10:58 +0200

    DocTypesComponent was adjusted

--- dvs-react/src/components/DocumentTypes/DocumentTypesComponent.js
+++ dvs-react/src/components/DocumentTypes/DocumentTypesComponent.js
@@ -4,21 +4,27 @@ import { Link } from "react-router-dom";
 
 const DocumentTypesComponent = props => {
   return (
-    <div className="card m-2">
-      <h5 className="card-header">"pirmas"</h5>
-      <div className="card-body">
-        <h5 className="card-title">antras</h5>
-        <p className="card-text">trecias</p>
+    <div className="container-fluid m-2">
+      <h3>Dokumentų tipai</h3>
+      <Link to={"/admin/newdoctype/"} className="btn btn-warning ">
+        Kurti naują dokumento tipą
+      </Link>
 
-        <Link to={"/admin/newdoctype/"} className="btn btn-warning m-2">
-          Kurti naują dokumento tipą
-        </Link>
-        <form>
-          <button type="submit" className="btn btn-primary m-2">
-            delete
-          </button>
-        </form>
-      </div>
+      {/* <div className="card m-2">
+       <h5 className="card-header">"pirmas"</h5>
+       <div className="card-body">
+         <h5 className="card-title">antras</h5>
+         <p className="card-text">trecias</p>
+
+         <Link to={"/admin/newdoctype/"} className="btn btn-warning m-2">
+           Kurti naują dokumento tipą
+         </Link>
+         <form>
+           <button type="submit" className="btn btn-primary m-2">
+             delete
+           </button>
+         </form>
+       </div>*/}
     </div>
   );
 };
