a2ea8c7e98ca0630f65d14595bdedcf6e33347f4
Paulius <paulius.ccs@gmail.com>
2019-02-09 11:51:58 +0200

    Looks like it works. Alert is launched. But the state.msg is logged out as false after all - wtf?

--- dvs-react/src/components/Forms/DocumentType/CreateDocumentTypeComponent.js
+++ dvs-react/src/components/Forms/DocumentType/CreateDocumentTypeComponent.js
@@ -3,21 +3,24 @@ import PropTypes from "prop-types";
 
 const CreateDocumentTypeComponent = props => {
   return (
-    <div className="container-fluid m-2">
-      <h3 className="display-6 ">Naujo dokumento tipo kūrimas</h3>
-      <h5>Dokumento tipo pavadinimas</h5>
-      <input
-        onChange={props.onTitleChange}
-        className="form-control col-4"
-        type="text"
-        placeholder="Įveskite dokumento tipo pavadinimą"
-      />
+    <div>
+      <div className="container-fluid m-2">
+        <h3 className="display-6 ">Naujo dokumento tipo kūrimas</h3>
+        <h5>Dokumento tipo pavadinimas</h5>
+        <input
+          onChange={props.onTitleChange}
+          className="form-control col-4"
+          type="text"
+          placeholder="Įveskite dokumento tipo pavadinimą"
+        />
 
-      <div className="form-group" />
+        {/* <div className="form-group" /> */}
 
-      <button onClick={props.onSubmit} className="btn btn-info">
-        Kurti
-      </button>
+        <button onClick={props.onSubmit} className="btn btn-info">
+          Kurti
+        </button>
+      </div>
+      <div className="container">{props.launchAlert}</div>
     </div>
   );
 };
--- dvs-react/src/components/Forms/DocumentType/CreateDocumentTypeContainer.js
+++ dvs-react/src/components/Forms/DocumentType/CreateDocumentTypeContainer.js
@@ -4,32 +4,88 @@ import axios from "axios";
 import CreateDocumentTypeComponent from "./CreateDocumentTypeComponent";
 
 class CreateDocumentTypeConatainer extends Component {
-  state = { title: "" };
+  state = {
+    title: "",
+    msg: false
+  };
 
   handleSubmit = () => {
+    // axios
+    //   .post("http://localhost:8081/api/doctypes", {
+    //     title: this.state.title
+    //   })
+    //   .then(response => {
+    //     console.log(response);
+    //     console.log("PO sekmingo submito state.sth");
+    //     // this.setState({ msg: true });
+    //   })
+    //   .catch(function(error) {
+    //     console.log(error);
+    //   });
+
     axios
       .post("http://localhost:8081/api/doctypes", {
         title: this.state.title
       })
-      .then(function(response) {
-        console.log(response);
+      .then(response => {
+        const uploadStatus = "Type was created successfully";
+        console.log("upload status >>>>>>>>>> ", uploadStatus);
+        this.setState({ msg: true });
       })
       .catch(function(error) {
+        //it works without catch block as well
         console.log(error);
+        if (error.response) {
+          //HTTP error happened
+          console.log(
+            "Upload error. HTTP error/status code=",
+            error.response.status
+          );
+        } else {
+          //some other error happened
+          console.log("Upload error. HTTP error/status code=", error.message);
+        }
       });
     console.log(">>>>>>>>>Submit happened");
     console.log("@@@@@@@@@@@@@@@ this.state.title >>>>>>>> ", this.state.title);
+    console.log("@@@@@@@@@@@@@@@56 this.state.msg >>>>>>>> ", this.state.msg);
   };
 
   handleTitleChange = e => {
     let documentTypeTitle = e.target.value;
     this.setState({ title: documentTypeTitle });
   };
+
+  launchAlert = () => {
+    if (this.state.msg) {
+      return (
+        <div className="alert alert-success alert-dismissible">
+          <button
+            onClick={this.handleCloseAlert}
+            href="#"
+            className="close"
+            data-dismiss="alert"
+            aria-label="close"
+          >
+            &times;
+          </button>
+          <strong>Sveikiname!</strong> Dokumento tipas {this.state.title}{" "}
+          sukurtas sėkmingai.
+        </div>
+      );
+    }
+    return null;
+  };
+  handleCloseAlert = () => {
+    this.setState({ msg: false });
+  };
+
   render() {
     return (
       <CreateDocumentTypeComponent
         onTitleChange={this.handleTitleChange}
         onSubmit={this.handleSubmit}
+        launchAlert={this.launchAlert()}
       />
     );
   }
