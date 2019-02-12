d26c051aeb7a0afa283e56f688a212df63a07c4f
Paulius <paulius.ccs@gmail.com>
2019-02-12 17:18:27 +0200

    UsersCOmponent and Container were introduced

---
+++ dvs-react/src/components/Users/UsersComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const UsersComponent = props => {
+  return (
+    <div className="container pl-0 ml-0">
+      <div className="row">
+        <div className="col-2">
+          {props.typeId}
+          {/* <Link to="">Linkas</Link> */}
+        </div>
+
+        <div className="col-2">
+          <p>{props.typeTitle}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UsersComponent;
---
+++ dvs-react/src/components/Users/UsersContainer.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import DocumentTypesComponent from "./DocumentTypesComponent";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+class UsersContainer extends Component {
+  state = {
+    docTypes: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8081/api/doctypes")
+      .then(response => {
+        this.setState({ docTypes: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    console.log(
+      "ComponentDidMount inside DocumentTYpesCOntainer >>>>>>>>>> this.state.docTypes>>>>.",
+      this.state.docTypes
+    );
+  }
+
+  render() {
+    var docTypesArrayToRender = this.state.docTypes.map(oneTypeObj => {
+      return (
+        <DocumentTypesComponent
+          key={oneTypeObj.id}
+          typeId={oneTypeObj.id}
+          typeTitle={oneTypeObj.title}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <div className="container-fluid m-2 ">
+          <h3 className="display-6">Dokumentų tipai</h3>
+          <Link to={"/admin/newdoctype/"} className="btn btn-warning mb-2">
+            Kurti naują dokumento tipą
+          </Link>
+          <div className="container pl-0 ml-0">
+            <div className="row">
+              <div className="col-2">
+                <p>
+                  {" "}
+                  <strong>Dokumento tipo ID</strong>
+                </p>
+                {/* <Link to="">Linkas</Link> */}
+              </div>
+
+              <div className="col-3">
+                <p>
+                  <strong>Dokumento tipo pavadinimas</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+          {docTypesArrayToRender}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UsersContainer;
