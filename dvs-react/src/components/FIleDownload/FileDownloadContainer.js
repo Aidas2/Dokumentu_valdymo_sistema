1507a94500706993c16482734142bb67fb45f763
Paulius <paulius.ccs@gmail.com>
2019-02-13 18:32:38 +0200

    FileDownloadContainer was introduced

---
+++ dvs-react/src/components/FIleDownload/FileDownloadContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class FileDownloadConatainer extends Component {
+  state = {
+    file: "",
+    error: "",
+    msg: ""
+  };
+  render() {
+    return null;
+  }
+}
+
+export default FileDownloadConatainer;
