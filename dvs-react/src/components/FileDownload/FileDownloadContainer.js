b1a8c3ee5dd0d700f7e67a081beb8246829c03f8
Paulius <paulius.ccs@gmail.com>
2019-02-13 18:49:59 +0200

    Working on UploadFile() method

--- dvs-react/src/components/FileDownload/FileDownloadContainer.js
+++ dvs-react/src/components/FileDownload/FileDownloadContainer.js
@@ -6,7 +6,30 @@ class FileDownloadConatainer extends Component {
     error: "",
     msg: ""
   };
+
+  onFileChange = event => {
+    this.setState({ file: event.target.files[0] });
+    event.preventDefault();
+    this.setState({ error: "", msg: "" });
+    if (!this.state.file) {
+      this.setState({ error: "Please upload a file" });
+      return;
+    }
+    if (this.state.file.size >= 2000000) {
+      this.setState({ error: "The file exeeds 2MB" });
+      return;
+    }
+    let data = new FormData();
+    data.append("file", this.state.file);
+    data.append("name", this.state.file.name);
+  };
+
+  uploadFile = e => {};
   render() {
+    console.log(
+      this.state,
+      "<<<<<<<<<<<<<<<<<<<,THis.state in render()------------------"
+    );
     return (
       <div className="App-intro">
         <h3>Upload a file</h3>
