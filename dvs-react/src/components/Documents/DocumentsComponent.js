85951e784bb960e50b1d9c39ba3677f4054db39d
Paulius <paulius.ccs@gmail.com>
2019-02-24 21:02:18 +0200

    DocumentCOmponent.js was created

---
+++ dvs-react/src/components/Documents/DocumentsComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const UsersComponent = props => {
+  var {
+    administrator,
+    emailAddress,
+    firstName,
+    hireDate,
+    lastName,
+    password,
+    username,
+    userId
+  } = props.userDetails;
+  return (
+    <div className="container pl-0 ml-0">
+      <div className="row">
+        <div className="col-2">
+          {userId}
+          {/* <Link to="">Linkas</Link> */}
+        </div>
+
+        <div className="col-2">
+          <p>{username}</p>
+        </div>
+        <div className="col-2">
+          <p>{firstName}</p>
+        </div>
+        <div className="col-2">
+          <p>{lastName}</p>
+        </div>
+        <div className="col-2">
+          <p>{emailAddress}</p>
+        </div>
+        <div className="col-2">
+          <p>{hireDate}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UsersComponent;
