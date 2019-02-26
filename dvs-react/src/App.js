2486f18c03bfb92bc23740963916da759b9a0246
Mindaugas <mindaugasrimdzius@gmail.com>
2019-02-26 12:12:54 +0200

    added footer in UI

--- dvs-react/src/App.js
+++ dvs-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import NavigationComponent from "./components/Navigation/NavigationComponent";
+import FooterComponent from "./components/Footer/FooterComponent";
 
 const App = props => {
   return (
     <React.Fragment>
       <NavigationComponent />
       {props.children}
+      <FooterComponent />
     </React.Fragment>
   );
 };
---
+++ dvs-react/src/components/Footer/FooterComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../images/home.png";
+
+class FooterComponent extends Component {
+  render() {
+    return <div class="footer">@ 2019 Copyright Bronza</div>;
+  }
+}
+
+export default FooterComponent;
