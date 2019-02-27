0a422ccdf795b0cd236a7792b2da8301e9672887
Paulius <paulius.ccs@gmail.com>
2019-02-27 19:03:52 +0200

    Looks like file ca be viewed vis Postman and URL. Still hardcoded

--- dvs-spring/src/main/java/lt/akademijait/bronza/controllers/FileManageController.java
+++ dvs-spring/src/main/java/lt/akademijait/bronza/controllers/FileManageController.java
@@ -90,7 +90,7 @@ public class FileManageController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
 
