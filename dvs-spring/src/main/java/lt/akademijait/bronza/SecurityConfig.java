6df95f910e3e142008920e7c4941f4b09be2032c
Vilius Zalys <you@example.com>
2019-03-01 00:59:22 +0200

    Security class added (inactive)

---
+++ dvs-spring/src/main/java/lt/akademijait/bronza/SecurityConfig.java
@@ -0,0 +1,15 @@
+package lt.akademijait.bronza;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//
+//
+//}
