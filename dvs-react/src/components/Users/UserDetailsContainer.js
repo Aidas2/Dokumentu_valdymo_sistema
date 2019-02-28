4d965c8243627c8ebef7aa77e6ff157a8e63aa17
Paulius <paulius.ccs@gmail.com>
2019-02-28 16:43:44 +0200

    One user info is gotten on reat. Still hardcoded

--- dvs-react/src/components/Users/UserDetailsContainer.js
+++ dvs-react/src/components/Users/UserDetailsContainer.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 import UserDetailsComponent from "./UserDetailsComponent";
+import axios from "axios";
 
 class UserDetailsContainer extends Component {
   state = {};
+
+  componentDidMount() {
+    axios({
+      url: "http://localhost:8081/api/users",
+      method: "GET",
+      params: {
+        username: "username1"
+      }
+    })
+      .then(response => {
+        // this.setState({ users: response.data });
+        console.log("response -------------- > ", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
     return <UserDetailsComponent />;
   }
