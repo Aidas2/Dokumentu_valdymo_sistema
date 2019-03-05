9317c92ab4e06e16e0530df6c57b6c6a1888da3b
Mindaugas <mindaugasrimdzius@gmail.com>
2019-03-05 19:33:35 +0200

    fixed console error

--- dvs-react/src/components/Administration/AdministrationContainer.js
+++ dvs-react/src/components/Administration/AdministrationContainer.js
@@ -26,7 +26,7 @@ class AdministrationContainer extends Component {
 
         <h5 className="display-6 normal-padding second-navigation-style ">
           <Link to={"/"}>
-            <img src={logo} width="20" height="10" />
+            <img src={logo} width="20" height="10" alt="logo icon" />
           </Link>
           &ensp;/ &ensp;
           <Link to={"/admin"} className="second-navigation">
