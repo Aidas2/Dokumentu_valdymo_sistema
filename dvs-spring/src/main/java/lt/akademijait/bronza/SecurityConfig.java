398c2fe684619249477badc0a16e08bae3839c72
Paulius <paulius.ccs@gmail.com>
2019-03-11 13:38:21 +0200

    Security was disabled. ROles is added, need controller for it, etc

--- dvs-spring/src/main/java/lt/akademijait/bronza/SecurityConfig.java
+++ dvs-spring/src/main/java/lt/akademijait/bronza/SecurityConfig.java
@@ -1,27 +1,27 @@
-package lt.akademijait.bronza;
-
-import lt.akademijait.bronza.security.SecurityEntryPoint;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled=true,prePostEnabled=true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private SecurityEntryPoint securityEntryPoint;
-    @Autowired
-    private UserDetailsService userService;
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.userDetailsService(userService);
-//auth.inMemoryAuthentication().withUser("uu")
-// .password("pp").roles("USER", "CALC");
-    }
-}
+//package lt.akademijait.bronza;
+//
+//import lt.akademijait.bronza.security.SecurityEntryPoint;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(securedEnabled=true,prePostEnabled=true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private SecurityEntryPoint securityEntryPoint;
+//    @Autowired
+//    private UserDetailsService userService;
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth.userDetailsService(userService);
+////auth.inMemoryAuthentication().withUser("uu")
+//// .password("pp").roles("USER", "CALC");
+//    }
+//}
