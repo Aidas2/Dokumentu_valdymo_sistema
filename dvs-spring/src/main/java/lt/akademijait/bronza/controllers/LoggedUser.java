ffe72df5b722cdf20c660994052f0adc4de47825
Paulius <paulius.ccs@gmail.com>
2019-03-13 20:26:26 +0200

    LoggedUser controller works. Username is returned

---
+++ dvs-spring/src/main/java/lt/akademijait/bronza/controllers/LoggedUser.java
@@ -0,0 +1,22 @@
+package lt.akademijait.bronza.controllers;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggedUser {
+    @RequestMapping(path = "/loggedUsername", method = RequestMethod.GET)
+    public String getLoggedInUsername() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            return currentUserName;
+        }
+        return "not logged";
+    }
+}
