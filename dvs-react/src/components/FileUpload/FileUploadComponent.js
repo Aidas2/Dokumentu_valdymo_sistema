755515d2823a98247a69b046c9ba0ae261b3ddd3
Mindaugas <mindaugasrimdzius@gmail.com>
2019-03-14 21:50:12 +0200

    fixed Documents upload menu

--- dvs-react/src/components/FileUpload/FileUploadComponent.js
+++ dvs-react/src/components/FileUpload/FileUploadComponent.js
@@ -47,42 +47,132 @@ class FileUploadComponent extends Component {
         <div className="container-fluid">
           <div className="row users-padding-bottom table-style-rounded">
             <div className="col-1 users-table-number-style  ">#</div>
-            <div className="col-2  users-table-middle-style ">Pavadinimas</div>
-            <div className=" col-2 users-table-middle-style">Tipas</div>
-            <div className="col-2    users-table-middle-style ">Būsena</div>
-            <div className="col-2  users-table-middle-style">Aprašymas</div>
-            <div className="col-3  users-table-action-style ">Veiksmai</div>
+            <div className="col-5  users-table-middle-style ">
+              Lauko pavadinimas
+            </div>
+            <div className=" col-6 users-table-action-style">
+              Įvedimo laukas
+            </div>
           </div>
-          <div className="">{55}</div>
+
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">1</div>
+            <div className="col-5 documents-table-size">
+              Dokumento pavadinimas
+            </div>
+            <div className="col-6 documents-table-size">
+              <input
+                className=" form-control form-control-sm italic-style"
+                placeholder="Įveskite dokumento pavadinimą"
+                type="text"
+                onChange={this.props.onDocumentTitle}
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">2</div>
+            <div className="col-5 documents-table-size">
+              Dokumento aprašymas
+            </div>
+            <div className="col-6 documents-table-size">
+              <input
+                className="form-control form-control-sm italic-style"
+                placeholder="Įveskite dokumento aprašymą"
+                type="text"
+                onChange={this.props.onDocumentDescription}
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">3</div>
+            <div className="col-5 documents-table-size">Dokumento tipas</div>
+            <div className="col-6 documents-table-size">
+              <DocumentTypesContainer
+                documentTypes={this.props.documentTypes}
+                onDocumentTypeChange={this.props.onDocumentTypeChange}
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">4</div>
+            <div className="col-5 documents-table-size">
+              Pagrindinis dokumentas
+            </div>
+            <div className="col-6 documents-table-size">
+              <div className="input-group   ">
+                <div className="input-group-prepend" />
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    onChange={this.props.onMainDocument}
+                    className="custom-file-input "
+                    id="inputGroupFile01"
+                  />
+                  <label
+                    className="custom-file-label italic-style"
+                    form="inputGroupFile01"
+                  >
+                    Pasirinkite pagrindinį dokumentą
+                  </label>
+                </div>
+              </div>
+              <span className="italic-style-small">
+                {this.props.documentName}
+              </span>
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">5</div>
+            <div className="col-5 documents-table-size">Priedai</div>
+            <div className="col-6 documents-table-size">
+              <div className="input-group ">
+                <div className="input-group-prepend" />
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    multiple
+                    onChange={this.props.onAttachments}
+                    className="custom-file-input "
+                    id="inputGroupFile02"
+                  />
+                  <label
+                    className="custom-file-label italic-style"
+                    form="inputGroupFile01"
+                  >
+                    Pasirinkite papildomus failus
+                  </label>
+                </div>
+              </div>
+              <span className="italic-style-small">
+                {this.props.attachmentsNames}
+              </span>
+            </div>
+          </div>
+          <div className="row ">
+            <div className="col-1 " />
+            <div className="col-5 " />
+            <div className="col-6 ">
+              <button
+                type="button"
+                className="btn btn-outline-success btn-sm document-button-style button-color"
+                onClick={this.props.onUpload}
+              >
+                Įkelti
+              </button>
+            </div>
+          </div>
+
+          <div className="container">{this.props.launchAlert}</div>
         </div>
 
-        <h2 className="display-6 normal-padding">
-          Dokumento įkėlimas
-          <div className="logo-info">
-            <img src={infoIcon} className="info-icon-style" alt="info icon" />
-            <span className="tooltiptext">
-              Šiame meniu pasirinkus norimus parametrus ir dokumentus, galima
-              įkelti dokumentus.
-            </span>
-          </div>
-        </h2>
-
-        <h5 className="display-6 normal-padding second-navigation-style ">
-          <Link to={"/"}>
-            <img src={logo} width="20" height="10" alt="logo icon" />
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={"/upload"} className="second-navigation">
-            Dokumento įkėlimas
-          </Link>
-        </h5>
-
-        <table className="table table-color-style table-style-rounded">
+        {/* <table className="table table-color-style table-style-rounded users-padding-bottom">
           <tbody>
             <tr>
-              <th className="col-1">#</th>
-              <th className="col-5">Lauko pavadinimas</th>
-              <th className="col-6">Įvedimo laukas</th>
+              <th className="col-1 users-table-number-style ">#</th>
+              <th className="col-5 users-table-middle-style">
+                Lauko pavadinimas
+              </th>
+              <th className="col-6 users-table-action-style">Įvedimo laukas</th>
             </tr>
             <tr>
               <td>
@@ -213,7 +303,7 @@ class FileUploadComponent extends Component {
             </tr>
           </tbody>
         </table>
-        <div className="container">{this.props.launchAlert}</div>
+        <div className="container">{this.props.launchAlert}</div> */}
       </div>
     );
   }
