27ab82e965876da224eb1293ee4fb1d2f9fea2e8
Paulius <paulius.ccs@gmail.com>
2019-03-14 13:14:58 +0200

    Login on UI works

--- dvs-react/src/components/login/LoginComponent.js
+++ dvs-react/src/components/login/LoginComponent.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
 const LoginComponent = (
-  { email, pass, onPassChange, onEmailChange, onSubmit },
+  { username, pass, onPassChange, onUsernameChange, onSubmit },
   context
 ) => {
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" value={email} onChange={onEmailChange} />
+      <input type="text" value={username} onChange={onUsernameChange} />
       <input type="password" value={pass} onChange={onPassChange} />
       <input type="submit" />
     </form>
--- dvs-react/src/components/login/LoginContainer.js
+++ dvs-react/src/components/login/LoginContainer.js
@@ -4,15 +4,20 @@ import LoginComponent from "./LoginComponent";
 axios.defaults.withCredentials = true; // leidzia dalintis cookies
 
 class LoginContainer extends Component {
-  onEmailChange = event => {
-    this.setState({ email: event.target.value });
+  state = {
+    username: "",
+    pass: ""
+  };
+
+  onUsernameChange = event => {
+    this.setState({ username: event.target.value });
   };
   onPassChange = event => {
     this.setState({ pass: event.target.value });
   };
   onSubmit = event => {
     let userData = new URLSearchParams();
-    userData.append("username", this.state.email);
+    userData.append("username", this.state.username);
     userData.append("password", this.state.pass);
     axios
       .post("http://localhost:8081/login", userData, {
@@ -28,8 +33,28 @@ class LoginContainer extends Component {
   };
 
   render() {
-    return <LoginComponent />;
+    console.log("this.state in render() ------------ ", this.state);
+    return (
+      <LoginComponent
+        username={this.state.username}
+        pass={this.state.pass}
+        onUsernameChange={this.onUsernameChange}
+        onPassChange={this.onPassChange}
+        onSubmit={this.onSubmit}
+      />
+    );
   }
+  onCalc = event => {
+    axios
+      .get("http://localhost:8081/calc?left=1&right=2")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    event.preventDefault();
+  };
 }
 
 export default LoginContainer;
