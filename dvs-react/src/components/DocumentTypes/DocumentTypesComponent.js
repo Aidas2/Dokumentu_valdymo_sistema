e0a668e5b8a47863fa6fcecb81a2c316fe14438e
Mindaugas <mindaugasrimdzius@gmail.com>
2019-03-16 14:52:22 +0200

    changed CocumentTypes UI

--- dvs-react/src/components/DocumentTypes/DocumentTypesComponent.js
+++ dvs-react/src/components/DocumentTypes/DocumentTypesComponent.js
@@ -2,16 +2,20 @@ import React from "react";
 
 const DocumentTypesComponent = props => {
   return (
-    <div className="container pl-0 ml-0">
-      <div className="row">
-        <div className="col-12 padding-normal">
-          {props.typeId}
-
-          {props.typeTitle}
-        </div>
-      </div>
+    <div className="row users-padding-bottom">
+      <div className="col-1 documents-table-size">{props.typeId}</div>
+      <div className="col-11 documents-table-size">{props.typeTitle}</div>
     </div>
   );
 };
 
 export default DocumentTypesComponent;
+
+// <div className="container pl-0 ml-0">
+//   <div className="row">
+//     <div className="col-12 padding-normal">
+//       {props.typeId}
+//       {props.typeTitle}
+//     </div>
+//   </div>
+// </div>
