ff52e6c7af4e22a4a5a1763f2ff2453f2de65e6c
Mindaugas <mindaugasrimdzius@gmail.com>
2019-03-16 16:35:27 +0200

    updated UserDetails and UpdateUser components in UI

--- dvs-react/src/components/Forms/User/UpdateUserComponent.js
+++ dvs-react/src/components/Forms/User/UpdateUserComponent.js
@@ -15,191 +15,177 @@ const UpdateUserComponent = props => {
     password,
     username
   } = props.userDetailsBeforeUpdate;
-
+  var linkToIndividualUserDetailsUpdate = "/admin/updateuser/" + username;
   return (
     <div>
-      <div className="container-fluid m-2">
-        <h2 className="display-6 normal-padding">
-          Vartotojo <stron>{username}</stron> informacijos atnaujinimas
-          <div className="logo-info">
-            <img src={infoIcon} className="info-icon-style" alt="info icon" />
-            <span className="tooltiptext">
-              Šiame meniu galima kurti naujus vartotojus.
-            </span>
+      <div className="container-fluid no-padding">
+        <div className="row justify-content-between no-padding ">
+          <div className=" col-12  normal-padding">
+            <h5 className="display-6  second-navigation-style ">
+              <Link to={"/"}>
+                <img
+                  className="logo-color"
+                  src={logo}
+                  width="40"
+                  height="20"
+                  alt="logo icon"
+                />
+              </Link>
+              &ensp;/ &ensp;
+              <Link to={"/admin"} className="second-navigation">
+                Administratoriaus rolė
+              </Link>
+              &ensp;/ &ensp;
+              <Link to={"/admin/users"} className="second-navigation">
+                Vartotojai
+              </Link>
+              &ensp;/ &ensp;
+              <Link
+                to={linkToIndividualUserDetailsUpdate}
+                className="second-navigation"
+              >
+                {username} redagavimas
+              </Link>
+            </h5>
+            <h2 className="display-6 ">
+              {username} redagavimas
+              <div className="logo-info">
+                <img
+                  src={infoIcon}
+                  className="info-icon-style"
+                  alt="info icon"
+                />
+                <span className="tooltiptext">
+                  Šiame lange galima redaguoti vartotojo informaciją
+                </span>
+              </div>
+            </h2>
           </div>
-        </h2>
-        <h5 className="display-6 normal-padding second-navigation-style ">
-          <Link to={"/"}>
-            <img src={logo} width="20" height="10" alt="logo icon" />
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={"/admin"} className="second-navigation">
-            Administratoriaus rolė
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={"/admin/users"} className="second-navigation">
-            Vartotojai
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={"/admin/newuser"} className="second-navigation">
-            Naujo vartotojo kūrimas
-          </Link>
-        </h5>
-        <table className="table table-striped table-style-rounded">
-          <tbody>
-            <tr>
-              <th className="col-1">#</th>
-              <th className="col-5">Lauko pavadinimas</th>
-              <th className="col-6">Įvedimo laukas</th>
-            </tr>
-            <tr>
-              <td>
-                <h6>1</h6>
-              </td>
-              <td>
-                <h6>Vardas</h6>
-              </td>
-              <td>
-                <input
-                  className=" form-control form-control-sm italic-style"
-                  placeholder={firstName}
-                  type="text"
-                  onChange={props.onFirstNameChange}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>2</h6>
-              </td>
-              <td>
-                <h6>Pavardė</h6>
-              </td>
+        </div>
 
-              <td>
-                <input
-                  className="form-control form-control-sm italic-style"
-                  placeholder={lastName}
-                  type="text"
-                  onChange={props.onLastNameChange}
-                />
-              </td>
-            </tr>
+        <div className="container-fluid">
+          <div className="row users-padding-bottom table-style-rounded">
+            <div className="col-1 users-table-number-style  ">#</div>
+            <div className="col-5  users-table-middle-style ">Parametras</div>
+            <div className=" col-6 users-table-action-style">Reikšmė</div>
+          </div>
 
-            <tr>
-              <td>
-                <h6>3</h6>
-              </td>
-              <td>
-                <h6>Elektroninio pašto adresas</h6>
-              </td>
-              <td>
-                <input
-                  className="form-control form-control-sm italic-style"
-                  placeholder={emailAddress}
-                  type="email"
-                  onChange={props.onEmailAddressChange}
-                />
-              </td>
-            </tr>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">1</div>
+            <div className="col-5 documents-table-size">Vardas</div>
+            <div className="col-6 documents-table-size">
+              <input
+                className=" form-control form-control-sm italic-style"
+                placeholder={firstName}
+                type="text"
+                onChange={props.onFirstNameChange}
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">2</div>
+            <div className="col-5 documents-table-size">Pavardė</div>
+            <div className="col-6 documents-table-size">
+              <input
+                className="form-control form-control-sm italic-style"
+                placeholder={lastName}
+                type="text"
+                onChange={props.onLastNameChange}
+              />
+            </div>
+          </div>
 
-            <tr>
-              <td>
-                <h6>4</h6>
-              </td>
-              <td>
-                <h6>Įdarbinimo data</h6>
-              </td>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">3</div>
+            <div className="col-5 documents-table-size">
+              Elektroninio pašto adresas
+            </div>
+            <div className="col-6 documents-table-size">
+              <input
+                className="form-control form-control-sm italic-style"
+                placeholder={emailAddress}
+                type="email"
+                onChange={props.onEmailAddressChange}
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">4</div>
+            <div className="col-5 documents-table-size">Įdarbinimo data</div>
+            <div className="col-6 documents-table-size">
+              <input
+                className="form-control form-control-sm italic-style"
+                placeholder={hireDate}
+                type="date"
+                onChange={props.onHireDateChange}
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">5</div>
+            <div className="col-5 documents-table-size">
+              Prisijungimo slaptažodis
+            </div>
+            <div className="col-6 documents-table-size">
+              <input
+                className="form-control form-control-sm italic-style"
+                placeholder={password}
+                p
+                type="password"
+                onChange={props.onPasswordChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">6</div>
+            <div className="col-5 documents-table-size">
+              Administratoriaus rolė
+            </div>
+            <div className="col-6 documents-table-size">
+              <select
+                onChange={props.onAdministratorChange}
+                className="col-6 form-control form-control-sm italic-style "
+                id="documentTypeSelect"
+              >
+                <option value="false">Ne</option>
+                <option value="true">Taip</option>
+              </select>
+              <br />
+              <span>
+                Po atnaujinimo vartotojui bus priskirta ši reikšmė:&ensp;
+                <strong> {props.mostRecentAdmintValue}</strong>
+              </span>
+            </div>
+          </div>
 
-              <td>
-                <input
-                  className="form-control form-control-sm italic-style"
-                  placeholder={hireDate}
-                  type="date"
-                  onChange={props.onHireDateChange}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>5</h6>
-              </td>
-              <td>
-                <h6>Prisijungimo slaptažodis</h6>
-              </td>
-
-              <td>
-                <input
-                  className="form-control form-control-sm italic-style"
-                  placeholder={password}
-                  p
-                  type="password"
-                  onChange={props.onPasswordChange}
-                  required
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>6</h6>
-              </td>
-              <td>
-                <h6>Administratoriaus rolė</h6>
-              </td>
-
-              <td>
-                <select
-                  onChange={props.onAdministratorChange}
-                  className="col-6 form-control form-control-sm italic-style "
-                  id="documentTypeSelect"
-                >
-                  <option value="false">Ne</option>
-                  <option value="true">Taip</option>
-                </select>
-                <br />
-                <span>
-                  Po atnaujinimo vartotojui bus priskirta ši reikšmė:&ensp;
-                  <strong> {props.mostRecentAdmintValue}</strong>
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>7</h6>
-              </td>
-              <td>
-                <h6>Vartotojo grupė</h6>
-              </td>
-
-              <td>
-                <UserGroupsContainer
-                  userGroups={props.userGroups}
-                  onUserGroupChange={props.onUserGroupChange}
-                />
-                <span className="italic-style-small">
-                  Pasirinktos grupės: {props.selectedUserGroupsTitles}
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6> </h6>
-              </td>
-              <td>
-                <h6> </h6>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  className="btn btn-outline-success btn-sm document-button-style"
-                  onClick={props.onSubmit}
-                >
-                  Atnaujinti
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">7</div>
+            <div className="col-5 documents-table-size">Vartotojo grupės</div>
+            <div className="col-6 documents-table-size list-style">
+              <UserGroupsContainer
+                userGroups={props.userGroups}
+                onUserGroupChange={props.onUserGroupChange}
+              />
+              <span className="italic-style-small">
+                Pasirinktos grupės: {props.selectedUserGroupsTitles}
+              </span>
+            </div>
+          </div>
+          <div className="row ">
+            <div className="col-1 " />
+            <div className="col-5 " />
+            <div className="col-6 ">
+              <button
+                type="button"
+                className="btn btn-outline-success m-2 button-color"
+                onClick={props.onSubmit}
+              >
+                Atnaujinti
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="container">{props.launchAlert}</div>
     </div>
--- dvs-react/src/components/Users/UserDetailsComponent.js
+++ dvs-react/src/components/Users/UserDetailsComponent.js
@@ -20,138 +20,111 @@ const UserDetailsComponent = props => {
 
   return (
     <div>
-      <div className="container-fluid m-2">
-        <h2 className="display-6 normal-padding">
-          {username}
-          <div className="logo-info">
-            <img src={infoIcon} className="info-icon-style" alt="info icon" />
-            <span className="tooltiptext">
-              Šiame lange yra pateikta vartotojo informacija.
-            </span>
+      <div className="container-fluid no-padding">
+        <div className="row justify-content-between no-padding ">
+          <div className=" col-8  normal-padding">
+            <h5 className="display-6  second-navigation-style ">
+              <Link to={"/"}>
+                <img
+                  className="logo-color"
+                  src={logo}
+                  width="40"
+                  height="20"
+                  alt="logo icon"
+                />
+              </Link>
+              &ensp;/ &ensp;
+              <Link to={"/admin"} className="second-navigation">
+                Administratoriaus rolė
+              </Link>
+              &ensp;/ &ensp;
+              <Link to={"/admin/users"} className="second-navigation">
+                Vartotojai
+              </Link>
+              &ensp;/ &ensp;
+              <Link
+                to={linkToIndividualUserDetails}
+                className="second-navigation"
+              >
+                {username}
+              </Link>
+            </h5>
+            <h2 className="display-6 ">
+              {username}
+              <div className="logo-info">
+                <img
+                  src={infoIcon}
+                  className="info-icon-style"
+                  alt="info icon"
+                />
+                <span className="tooltiptext">
+                  Šiame lange yra pateikta vartotojo informacija.
+                </span>
+              </div>
+            </h2>
           </div>
-        </h2>
-        <h5 className="display-6 normal-padding second-navigation-style ">
-          <Link to={"/"}>
-            <img src={logo} width="20" height="10" alt="logo icon" />
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={"/admin"} className="second-navigation">
-            Administratoriaus rolė
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={"/admin/users"} className="second-navigation">
-            Vartotojai
-          </Link>
-          &ensp;/ &ensp;
-          <Link to={linkToIndividualUserDetails} className="second-navigation">
-            {username}
-          </Link>
-        </h5>
+          <div className=" col-4  normal-padding left-align ">
+            <Link
+              to={linkToIndividualUsserUpdateContainer}
+              className="btn btn-outline-success m-2 button-color "
+            >
+              Redaguoti vartotoją
+            </Link>
+          </div>
+        </div>
 
-        <table className="table table-striped table-style-rounded">
-          <tbody>
-            <tr>
-              <th scope="col-1">#</th>
-              <th scope="col-5">Parametras</th>
-              <th scope="col-6">Reikšmė</th>
-            </tr>
-            <tr>
-              <td>
-                <h6>1</h6>
-              </td>
-              <td>
-                <h6>Vardas</h6>
-              </td>
-              <td>
-                <h6>{firstName}</h6>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>2</h6>
-              </td>
-              <td>
-                <h6>Pavardė</h6>
-              </td>
-              <td>
-                <h6>{lastName}</h6>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>3</h6>
-              </td>
-              <td>
-                <h6>Unikalus vardas sistemoje</h6>
-              </td>
-              <td>
-                <h6>{username}</h6>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>4</h6>
-              </td>
-              <td>
-                <h6>Elektroninio pašto adresas</h6>
-              </td>
-              <td>
-                <h6>{emailAddress}</h6>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>5</h6>
-              </td>
-              <td>
-                <h6>Įdarbinimo data</h6>
-              </td>
-              <td>
-                <h6>{hireDate}</h6>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>6</h6>
-              </td>
-              <td>
-                <h6>Ar šis klientas turi dministratoriaus rolę?</h6>
-              </td>
-              <td>
-                <h6>{administrator}</h6>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h6>7</h6>
-              </td>
-              <td>
-                <h6>Vartotojo grupės</h6>
-              </td>
-              <td>
-                <h6 className="list-style">{props.userGroups}</h6>
-              </td>
-            </tr>{" "}
-            <tr>
-              <td>
-                <h6 />
-              </td>
-              <td>
-                <h6>
-                  <Link
-                    to={linkToIndividualUsserUpdateContainer}
-                    className="btn btn-secondary"
-                  >
-                    Redaguoti vartotoją
-                  </Link>
-                </h6>
-              </td>
-              <td>
-                <h6 className="list-style" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="container-fluid">
+          <div className="row users-padding-bottom table-style-rounded">
+            <div className="col-1 users-table-number-style  ">#</div>
+            <div className="col-5  users-table-middle-style ">Parametras</div>
+            <div className=" col-6 users-table-action-style">Reikšmė</div>
+          </div>
+
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">1</div>
+            <div className="col-5 documents-table-size">Vardas</div>
+            <div className="col-6 documents-table-size">{firstName}</div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">2</div>
+            <div className="col-5 documents-table-size">Pavardė</div>
+            <div className="col-6 documents-table-size">{lastName}</div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">3</div>
+            <div className="col-5 documents-table-size">
+              Unikalus vardas sistemoje
+            </div>
+            <div className="col-6 documents-table-size">{username}</div>
+          </div>
+
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">4</div>
+            <div className="col-5 documents-table-size">
+              Elektroninio pašto adresas
+            </div>
+            <div className="col-6 documents-table-size">{emailAddress}</div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">5</div>
+            <div className="col-5 documents-table-size">Įdarbinimo data</div>
+            <div className="col-6 documents-table-size">{hireDate}</div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">6</div>
+            <div className="col-5 documents-table-size">
+              Ar šis klientas turi administratoriaus rolę
+            </div>
+            <div className="col-6 documents-table-size">{administrator}</div>
+          </div>
+          <div className="row users-padding-bottom">
+            <div className="col-1 documents-table-size">7</div>
+            <div className="col-5 documents-table-size">Vartotojo grupės</div>
+            <div className="col-6 documents-table-size list-style">
+              {props.userGroups}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
