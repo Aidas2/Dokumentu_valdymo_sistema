fc2569852861afd1a7f208bdd821a5e5bc62ae9e
Paulius <paulius.ccs@gmail.com>
2019-03-18 18:14:53 +0200

    ServicesContext.providers was added to index.js

--- dvs-react/src/index.js
+++ dvs-react/src/index.js
@@ -26,77 +26,86 @@ import UpdateUserContainer from "./components/Forms/User/UpdateUserContainer";
 import LoginContainer from "./components/login/LoginContainer";
 import LogoutContainer from "./components/login/LogoutContainer";
 import UpdateUserGroupContainer from "./components/Forms/UserGroups/UpdateUserGroupContainer";
+import ServicesContext from "./components/context/ServicesContext";
 
 // const urlToGetDocs = "http://localhost:8081/api/docs";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App>
-      <Switch>
-        {/* <Route exact path="/" component={LandingPageContainer} /> */}
-        <Route exact path="/" component={LoginContainer} />
-        <Route exact path="/logout" component={LogoutContainer} />
-        <Route exact path="/usergroups" component={UserGroupsContainer} />
-        <Route exact path="/usergroups/:id" />
-        <Route exact path="/admin" component={AdministrationContainer} />
-        <Route exact path="/upload" component={FileUploadContainer} />
-        <Route exact path="/download" component={FileDownloadConatainer} />
-        <Route
-          exact
-          path="/admin/newdoctype"
-          component={CreateDocumentTypeConatainer}
-        />
-        <Route
-          exact
-          path="/admin/doctypes"
-          component={DocumentTypesContainer}
-        />{" "}
-        <Route exact path="/admin/newuser" component={CreateUserContainer} />
-        <Route exact path="/admin/users" component={UsersContainer} />
-        <Route exact path="/statistics" component={StatisticsContainer} />
-        <Route
-          exact
-          path="/admin/updateuser/:username"
-          component={UpdateUserContainer}
-        />
-        <Route
-          exact
-          path="/admin/updategroup/:title"
-          component={UpdateUserGroupContainer}
-        />
-        {/* <Route
+    <ServicesContext.Provider
+      value={{ userFromContext: { name: "Paulius pagaliau pasikeite" } }}
+    >
+      <App>
+        <Switch>
+          {/* <Route exact path="/" component={LandingPageContainer} /> */}
+          <Route exact path="/" component={LoginContainer} />
+          <Route exact path="/logout" component={LogoutContainer} />
+          <Route exact path="/usergroups" component={UserGroupsContainer} />
+          <Route exact path="/usergroups/:id" />
+          <Route exact path="/admin" component={AdministrationContainer} />
+          <Route exact path="/upload" component={FileUploadContainer} />
+          <Route exact path="/download" component={FileDownloadConatainer} />
+          <Route
+            exact
+            path="/admin/newdoctype"
+            component={CreateDocumentTypeConatainer}
+          />
+          <Route
+            exact
+            path="/admin/doctypes"
+            component={DocumentTypesContainer}
+          />{" "}
+          <Route exact path="/admin/newuser" component={CreateUserContainer} />
+          <Route exact path="/admin/users" component={UsersContainer} />
+          <Route exact path="/statistics" component={StatisticsContainer} />
+          <Route
+            exact
+            path="/admin/updateuser/:username"
+            component={UpdateUserContainer}
+          />
+          <Route
+            exact
+            path="/admin/updategroup/:title"
+            component={UpdateUserGroupContainer}
+          />
+          {/* <Route
           exact
           path="/admin/docs"
           render={props => (
             <DocumentsContainer {...props} requestUrl={urlToGetDocs} />
           )}
         /> */}
-        <Route exact path="/admin/docs" component={DocumentsContainer} />
-        <Route
-          exact
-          path="/admin/docs/:id"
-          component={DocumentDetailsContainer}
-        />
-        <Route exact path="/docs" component={DocumentsContainer} />
-        <Route exact path="/testing" component={RenderResponse} />
-        <Route exact path="/admin/usergroups" component={UserGroupsContainer} />
-        <Route
-          exact
-          path="/admin/usergroups/:title"
-          component={UserGroupDetailsContainer}
-        />
-        <Route
-          exact
-          path="/admin/users/:username"
-          component={UserDetailsContainer}
-        />
-        <Route
-          exact
-          path="/admin/newusergroup"
-          component={CreateUserGroupsContainer}
-        />
-      </Switch>
-    </App>
+          <Route exact path="/admin/docs" component={DocumentsContainer} />
+          <Route
+            exact
+            path="/admin/docs/:id"
+            component={DocumentDetailsContainer}
+          />
+          <Route exact path="/docs" component={DocumentsContainer} />
+          <Route exact path="/testing" component={RenderResponse} />
+          <Route
+            exact
+            path="/admin/usergroups"
+            component={UserGroupsContainer}
+          />
+          <Route
+            exact
+            path="/admin/usergroups/:title"
+            component={UserGroupDetailsContainer}
+          />
+          <Route
+            exact
+            path="/admin/users/:username"
+            component={UserDetailsContainer}
+          />
+          <Route
+            exact
+            path="/admin/newusergroup"
+            component={CreateUserGroupsContainer}
+          />
+        </Switch>
+      </App>
+    </ServicesContext.Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
