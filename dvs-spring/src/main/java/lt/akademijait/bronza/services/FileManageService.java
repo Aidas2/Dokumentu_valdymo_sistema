8eec64080ad98aba14745604bfd189f60b98a4d7
Paulius <paulius.ccs@gmail.com>
2019-03-20 17:49:12 +0200

    Attachment names are given corrctly

--- dvs-spring/src/main/java/lt/akademijait/bronza/services/FileManageService.java
+++ dvs-spring/src/main/java/lt/akademijait/bronza/services/FileManageService.java
@@ -80,6 +80,7 @@ public class FileManageService {
         newDocument.setTitle(documentCreateCommand.getTitle());
         newDocument.setDescription(documentCreateCommand.getDescription());
         Set<Attachment> attachments = new HashSet<>();
+        int attachmentNumber = 1;
 
 
         for (int i = 0; i < files.length; i++) {
@@ -87,8 +88,8 @@ public class FileManageService {
             File userDirectory = new File(currentAbsolutePath + fileSeparator + "uploaded-files" + fileSeparator
                     + getLoggedInUsername());
             userDirectory.mkdirs();
+
             if (files[i].getContentType().equalsIgnoreCase("application/pdf")) {
-                int attachmentNumber = 1;
 
                 File fileToSave = new File(userDirectory, userID + "-" +
                         (i == 0 ? "" : "att" + attachmentNumber + "-") + getCurrentLocalDateTimeStamp() + "-"
