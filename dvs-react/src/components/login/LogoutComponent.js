1642f82322daa8b35e540b3a0e30aa497d7303b6
Paulius <paulius.ccs@gmail.com>
2019-03-21 17:49:37 +0200

    Nustatymai was removed from logout options

--- dvs-react/src/components/login/LogoutComponent.js
+++ dvs-react/src/components/login/LogoutComponent.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 const LogoutComponent = props => {
   return (
     <div className="dropdown-content">
-      <a href="https://www.google.com/">Nustatymai</a>
       <Link to="" onClick={props.onClick}>
         Atsijungti
       </Link>
